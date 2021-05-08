0a2891a335f029150794483b2fdfbdff06f22089
abhay-081060 <76918911+abhay-081060@users.noreply.github.com>
2021-05-08 16:16:49 +0530

    anagram2

--- hero3/src/SOLBTY.java
+++ hero3/src/SOLBTY.java
@@ -4,38 +4,18 @@ import java.io.InputStreamReader;
 
 public class SOLBTY {
     public static void main(String[] args) throws IOException {
-        InputStreamReader r= new InputStreamReader(System.in);
-        BufferedReader br=new BufferedReader(r);
-        int T=Integer.parseInt(br.readLine());
-        String b=br.readLine();
-        System.out.println(b);
-        int arr[]=new int[3];
-        try {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String b = br.readLine();
+            System.out.println(b);
+            int arr[] = new int[3];
             arr[0] = Integer.parseInt(b.substring(0, b.indexOf(' ')));
-        }
-        catch (Exception e) {
-            System.out.println("exception1");
-        }
-        b=b.substring(b.indexOf(' ')+1);
-        try {
+            b = b.substring(b.indexOf(' ') + 1);
             arr[1] = Integer.parseInt(b.substring(0, b.indexOf(' ')));
+            b = b.substring(b.indexOf(' ') + 1);
+            arr[2] = Integer.parseInt(b.substring(0));
         }
-        catch (Exception c)
-        {
-            System.out.println("exception2");
-        }
-        b=b.substring(b.indexOf(' ')+1);
-        try {
-            arr[2] = Integer.parseInt(b.substring(' ') + 1);
-        }
-        catch(Exception d)
-        {
-            System.out.println("exception3");
-        }
-        for(int i=0;i<3;i++)
-        {
-            System.out.println(arr[i]);
-        }
-
     }
 }
